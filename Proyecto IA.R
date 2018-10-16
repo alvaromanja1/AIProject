72561c3b0047e60428ae09b347f6bafbd53bc8d2
alvaromanja1 <alvaromanja1@opendeusto.es>
2018-10-16 17:58:00 +0200

    Deleted rows < 2010

--- Proyecto IA.R
+++ Proyecto IA.R
@@ -7,7 +7,7 @@ cat("\014")
 # Otro punto es establecer el working directory, con este comando hacemos que 
 # éste sea igual a aquél en el que se encuentra el script
 # Si es tu 1ª vez con R, tendrás que instalar el paquete rstudioapim
-path = setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Comprobamos que está correcto
 #getwd()
 
@@ -21,11 +21,18 @@ drivers = read.csv("csv/drivers.csv")
 driverStandings = read.csv("csv/driverStandings.csv")
 races = read.csv("csv/races.csv")
 results = read.csv("csv/results.csv")
-circuits = read.csv("csv/circuits.csv")
+
+drivers = drivers[, !(colnames(drivers) %in% c("number","code", "forename", "surname", "dob", "nationality", "url"))]
+driverStandings = driverStandings[, !(colnames(driverStandings) %in% c("positionText","driverStandingsId"))]
+races = races[, !(colnames(races) %in% c("round","date", "time", "url"))]
+results = results[, !(colnames(results) %in% c("round","date", "time", "url"))]
 
 #combinar csv's
 driveStands = merge(drivers, driverStandings, by = "driverId")
-driverResults = merge(driveStands, results, by = "driverId")
+driverResults = merge(driveStands, races, by = "raceId")
 
-#Borramos las columnas que no nos hagan falta
-driverResults <- driverResults[, !(colnames(driverResults) %in% c("fastestLapTime","fastestLapSpeed", "fastestLap", "time", "constructorId", "points.x", "position.x", "positionText.x"))]
+#elimina las filas cuyo año sea menor que 2010 debido a que ese año cambió el sistema de puntuación
+#y el resultado no sería homogéneo
+driverResults = driverResults[driverResults$year > "2009",]
+
+
