532f8c2df6a2f417c5431bdc9d6caf12d0119722
alvaromanja1 <alvaromanja1@opendeusto.es>
2018-10-16 15:15:22 +0200

    deleted unnecessary columns

--- Proyecto IA.R
+++ Proyecto IA.R
@@ -7,7 +7,7 @@ cat("\014")
 # Otro punto es establecer el working directory, con este comando hacemos que 
 # éste sea igual a aquél en el que se encuentra el script
 # Si es tu 1ª vez con R, tendrás que instalar el paquete rstudioapim
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+path = setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # Comprobamos que está correcto
 #getwd()
 
@@ -25,4 +25,7 @@ circuits = read.csv("csv/circuits.csv")
 
 #combinar csv's
 driveStands = merge(drivers, driverStandings, by = "driverId")
+driverResults = merge(driveStands, results, by = "driverId")
 
+#Borramos las columnas que no nos hagan falta
+driverResults <- driverResults[, !(colnames(driverResults) %in% c("fastestLapTime","fastestLapSpeed", "fastestLap", "time", "constructorId", "points.x", "position.x", "positionText.x"))]
